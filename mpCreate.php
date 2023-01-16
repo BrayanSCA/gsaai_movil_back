419b8553f31d3766f893f2f7e0bfe96a4c0ccac2
BrayanSCA <brayansca93@gmail.com>
2023-01-15 20:18:56 -0500

    últimas correcciones listas para entregar, prodcuto no completado

--- mpCreate.php
+++ mpCreate.php
@@ -8,7 +8,7 @@ $nom = $datos['nombre_mp'];
 $rel = $datos['relacion_cn'];
 
 if ($datos) {
-    $q = mysqli_query($conn, "INSERT INTO `materias_primas` (`cod_mp`,`nombre_mp`,`stock`,`relacion_cn`) VALUES ('$cod','$nom','$rel')");
+    $q = mysqli_query($conn, "INSERT INTO `materias_primas` (`cod_mp`,`nombre_mp`,`relacion_cn`) VALUES ('$cod','$nom','$rel')");
 
     if ($q) {
         http_response_code(201);
